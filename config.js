4f74bc720a00c9c2afa3af433b5c4d77fea470dc
Cameron-Townsend <cgt805@gmail.com>
2025-10-28 22:20:18 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Fill this with your Apps Script Web App URL ending in /exec
 window.APP_CONFIG = {
-  API: 'https://script.google.com/macros/s/AKfycbzrg4fq-bppa7cB4UpIc2bi_iqsYksL9gnN1gg5570ZJzh6t9eIw7N4Ltj7ioaiRB2qZw/exec'
+  API: 'https://script.google.com/macros/s/AKfycbw5Zs0CCfdKVnIAf_1pPppwoxpURzKJwWcT0cykBcE38YAvB1n--ANyyRPKLvGKEAU0/exec'
 };
